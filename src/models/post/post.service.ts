cdcbc592b09a50a5d39522b965172fcb687473e9
mdabdurrahman07 <jamilrahman2001@gmail.com>
2026-06-29 23:04:34 +0600

    dynamic orderBy implemented

--- src/models/post/post.service.ts
+++ src/models/post/post.service.ts
@@ -1,3 +1,4 @@
+import { commentService } from "./../comments/comment.service";
 import { CommentStatus, PostStatus } from "../../../generated/prisma/enums";
 import { prisma } from "../../lib/prisma";
 import {
@@ -17,7 +18,14 @@ const createPostInDB = async (payload: ICreatePostPayload, userId: string) => {
 };
 
 const fetchAllPostsFromDB = async (query: IPostQuery) => {
-  console.log(query);
+  // as query always string that's making here a number variable of limit page and skip
+
+  const limit = query.limit ? Number(query.limit) : 10;
+  const page = query.page ? Number(query.page) : 1;
+  const skip = (page - 1) * limit;
+  const sortBy = query.sortBy ? query.sortBy : "createdAt";
+  const sortOrder = query.sortOrder ? query.sortOrder : "desc";
+
   const result = await prisma.post.findMany({
     // pagination with limit or take and skip or page
     // take: 2, //limit
@@ -58,6 +66,14 @@ const fetchAllPostsFromDB = async (query: IPostQuery) => {
       ],
     },
 
+    // actual pagination
+
+    take: limit,
+    skip: skip,
+
+    orderBy: {
+      [sortBy]: sortOrder,
+    },
     include: {
       author: {
         omit: {
