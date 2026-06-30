1234da16e5ba73f64f6fe3c618a7a4707ab7dd88
mdabdurrahman07 <jamilrahman2001@gmail.com>
2026-06-30 23:50:58 +0600

    stripe checkout service implemented and prisma migrated and generated

--- src/models/subscription/subscription.controller.ts
+++ src/models/subscription/subscription.controller.ts
@@ -1,8 +1,20 @@
+import httpStatus from "http-status";
 import { NextFunction, Request, Response } from "express";
 import catchAsync from "../../util/catchAsync";
+import { subsService } from "./subscription.service";
+import { sendResponse } from "../../util/sendResponse";
 
 const createCheckoutSession = catchAsync(
-  async (req: Request, res: Response, next: NextFunction) => {},
+  async (req: Request, res: Response, next: NextFunction) => {
+    const userId = req.user?.id as string;
+    const result = await subsService.createCheckoutSessionService(userId);
+    sendResponse(res, {
+      success: true,
+      statusCode: httpStatus.OK,
+      message: "Checkout Successful",
+      data: result,
+    });
+  },
 );
 
 export const subsController = {
--- src/models/subscription/subscription.service.ts
+++ src/models/subscription/subscription.service.ts
@@ -1,4 +1,52 @@
-const createCheckoutSessionService = async () => {};
+import config from "../../config/dotenv.config";
+import { prisma } from "../../lib/prisma";
+import { stripe } from "../../lib/stripe";
+
+const createCheckoutSessionService = async (userId: string) => {
+  const transactionResult = await prisma.$transaction(async (tx) => {
+    const user = await tx.user.findUniqueOrThrow({
+      where: {
+        id: userId,
+      },
+      include: {
+        subscription: true,
+      },
+    });
+
+    // trying from db to take the id
+    let stripeCustomerId = user.subscription?.stripeCustomerId;
+
+    if (!stripeCustomerId) {
+      // create stripe customer
+      const customer = await stripe.customers.create({
+        email: user.email,
+        name: user.name,
+        metadata: { userId: user.id },
+      });
+
+      stripeCustomerId = customer.id;
+    }
+    // create stripe checkoutSession
+    const session = await stripe.checkout.sessions.create({
+      line_items: [
+        {
+          price: config.stripe_price_id,
+          quantity: 1,
+        },
+      ],
+      mode: "subscription",
+      customer: stripeCustomerId,
+      payment_method_types: ["card"],
+      success_url: `${config.app_url}/premium?success=true`,
+      cancel_url: `${config.app_url}/payment?success=false`,
+      metadata: { userId: user.id },
+    });
+    return session.url;
+  });
+  return {
+    payment_url: transactionResult,
+  };
+};
 
 export const subsService = {
   createCheckoutSessionService,
