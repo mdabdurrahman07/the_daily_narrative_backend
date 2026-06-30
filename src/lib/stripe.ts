ddee7b7c6a06ac1849e37ac515b95df97d5ea436
mdabdurrahman07 <jamilrahman2001@gmail.com>
2026-06-30 21:54:53 +0600

    env example file updated and subscription model controller and service file created

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -1,4 +1,4 @@
 import Stripe from "stripe";
 import config from "../config/dotenv.config";
 
-const stripe = new  Stripe(config.stripe_secret_key)
+export const stripe = new Stripe(config.stripe_secret_key);
